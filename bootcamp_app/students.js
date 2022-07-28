b6c62af7c48399f3f8cdaf3c3f2c4b2bfe6c95fb
Archana Agrawal <archana.agrawal3@outlook.com>
2022-07-27 21:35:18 -0400

    add queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,19 +7,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.connect()
+pool.connect(() => console.log('connected'))
 
-const userInput = process.argv;
+const cohort = `${process.argv[2]}%`;
+const limit = process.argv[3];
 
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${userInput[2]}%'
-LIMIT ${userInput[3]};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, [cohort, limit])
 .then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
+  console.log(res.rows)
 });
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,17 +9,17 @@ const pool = new Pool({
 
 pool.connect();
 
-process.argv;
+const userInput = process.argv[2];
 
 pool.query(`
   SELECT cohorts.name AS cohort, teachers.name AS teacher 
   FROM teachers RIGHT JOIN assistance_requests ON teachers.id = teacher_id
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name LIKE '${process.argv[2]}'
+  WHERE cohorts.name LIKE $1
   GROUP BY cohort, teacher
   ORDER BY teacher;
-`)
+`, [userInput])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
